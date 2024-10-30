dabaa6999b70f80589e664462de5fc00b162e37b
oumadalmas <101338113+oumadalmas@users.noreply.github.com>
2024-10-30 16:25:20 +0300

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,14 @@
+# question 2
+name = input("What's your name? ")
+
+# question 3
+age = input("What's your age?")
+
+# question 4
+location = input("What's is your location?")
+
+# question 5
+# Print a personalized message
+print("Hello [name], you are [age] years old and live in [location].")
+
+
